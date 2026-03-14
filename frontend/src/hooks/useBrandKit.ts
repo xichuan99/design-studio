d21a8d368b61f7d75551888d66bb0cf0f63e09e9
clarinovist <nugroho.pramono@gmail.com>
2026-03-14 12:02:08 +0700

    fix: resolve infinite rendering loop in useBrandKit hook

--- frontend/src/hooks/useBrandKit.ts
+++ frontend/src/hooks/useBrandKit.ts
@@ -21,7 +21,8 @@ export function useBrandKit() {
         } finally {
             setIsLoading(false);
         }
-    }, [api]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() => {
         fetchBrandKits();
